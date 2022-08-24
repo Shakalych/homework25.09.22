d9f44d5c0a0580c86f0d7d8752187d9f4fd40133
Shakalych <madstabr@gmail.com>
2022-08-25 04:17:35 +0500

    Update Program.cs

--- Exam2/Program.cs
+++ Exam2/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
 Console.Write("Введи число: ");
 int x = Convert.ToInt32(Console.ReadLine());
 string xT = Convert.ToString(x);
